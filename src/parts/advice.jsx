bfe5576bd043d798ac1ab2c27c19a0e2880e70ef
AshkanTavangar <ashkantavangar7@gmail.com>
2022-07-20 22:52:37 +0430

    Get useFetch data and render it

--- src/parts/advice.jsx
+++ src/parts/advice.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useFetch } from '../fetchApi/useFetch';
 import "./advice.css";
 
 export default function Advice() {
+
+
+
+  const url = "https://api.adviceslip.com/advice";
+  const {Data,loading , fetchApi} = useFetch(url);
+
+
+
   return (
     <div className='advice-box'>
-        <p>Advice</p>
-        <button>Refresh</button>
+        <p>{loading?"loading...":Data}</p>
+        <button onClick={fetchApi}>Refresh</button>
     </div>
   )
 }
